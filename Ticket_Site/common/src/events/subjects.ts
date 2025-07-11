b76cbf5652f62d5af25d1a6395cde7dbf73ca3d4
TayabAmir <amirtayyab8@gmail.com>
2025-07-11 11:08:21 +0500

    Updated common module to handle payment events

--- Ticket_Site/common/src/events/subjects.ts
+++ Ticket_Site/common/src/events/subjects.ts
@@ -3,5 +3,6 @@ export enum Subjects {
     TicketUpdated = "ticket:updated",
     OrderCreated = "order:created",
     OrderCancelled = "order:cancelled",
-    ExpirationComplete = "expiration:completed"
+    ExpirationComplete = "expiration:completed",
+    PaymentCreated = "payment:created"
 }
